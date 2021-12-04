1f0905f1fa0444a78b9ad080f4544d7a51f9c0f7
samueltober <samuel2@kth.se>
2021-12-04 21:18:17 +0100

    fully solve day_3

--- day_3.py
+++ day_3.py
@@ -18,6 +18,46 @@ def get_gamma_rate(list_of_binary: list):
     return gamma_rate
 
 
+def get_oxygen_rating(list_of_binary: list):
+    length_of_binary = len(list_of_binary[0])
+
+    for i in range(length_of_binary):
+        list_of_binary = get_dominating_bit_at_index(list_of_binary, i, type='oxygen')
+
+        if len(list_of_binary) == 1:
+            return list_of_binary[-1]
+
+    return list_of_binary[-1]
+
+
+def get_co2_rating(list_of_binary: list):
+    length_of_binary = len(list_of_binary[0])
+
+    for i in range(length_of_binary):
+        list_of_binary = get_dominating_bit_at_index(list_of_binary, i, type='co2')
+        if len(list_of_binary) == 1:
+            return list_of_binary[-1]
+
+
+def get_dominating_bit_at_index(list_of_binary: list, position: int, type: str):
+    max_dict = {'1': [], '0': []}
+
+    for number in list_of_binary:
+        bit_at_pos = number[position]
+        max_dict[bit_at_pos].append(number)  # Store all numbers starting with 1 or 0
+
+    if type == 'oxygen':
+        if len(max_dict['1']) >= len(max_dict['0']):
+            return max_dict['0']
+        else:
+            return max_dict['1']
+    elif type == 'co2':
+        if len(max_dict['1']) >= len(max_dict['0']):
+            return max_dict['1']
+        else:
+            return max_dict['0']
+
+
 def get_complement(binary_number: str):
     complement = ""
     for bit in binary_number:
@@ -33,4 +73,10 @@ if __name__ == '__main__':
     gamma_rate = get_gamma_rate(list_of_binary)
     epsilon_rate = get_complement(gamma_rate)
 
-    print(int(gamma_rate, 2) * int(epsilon_rate, 2))
+    print(f"Power consumption: {int(gamma_rate, 2) * int(epsilon_rate, 2)}")
+
+    # Part 2
+    oxygen_rating = int(get_oxygen_rating(list_of_binary), 2)
+    co2_rating = int(get_co2_rating(list_of_binary), 2)
+
+    print(f"Life support rating: {oxygen_rating * co2_rating}")
